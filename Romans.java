7f993fed4d4e33889064c6c28a6e33710beee74c
OjasKhonde <ojasvkhonde@gmail.com>
2024-09-19 16:48:01 +0000

    RomanNumbers

--- Romans.java
+++ Romans.java
@@ -1,23 +1,38 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Ojas Khonde
 
 import java.util.*;
 
 public class Romans
 {
-	private Map<String, Integer> m;
+	private Map<String, Integer> map;
 	
 	public Romans()
 	{
-		m = new TreeMap<String, Integer>();
+		map = new TreeMap<String, Integer>();
 		
-		//complete your map				
+		//complete your map
+		map.put("I",1);	
+		map.put("V",5);
+		map.put("X",10);	
+		map.put("L",50);
+		map.put("C",100);
+		map.put("D",500);
+		map.put("M",1000);	
+	
 	}
 	
 	public int getNumber( String s )
 	{
 		//complete this method
+		if(map.containsKey(s))
+		{
+			return map.get(s);
+		}
+		else
+		{
+			return -1;
+		}
 	}
-
 }
--- RomansRunner.java
+++ RomansRunner.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Ojas Khonde
 
 public class RomansRunner
 {
